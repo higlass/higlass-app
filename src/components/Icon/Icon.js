bf9bbdd41d1a789c4d53649fba5ca7c806c6688e
Fritz Lekschas <service@lekschas.de>
2017-07-28 12:32:01 -0400

    Add wrapper element. Fix getting view-box and fill-rule

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -5,10 +5,10 @@ import icons, { WARNING } from '../../configs/icons';
 
 const getSvg = id => wrapHtml(icons[id] ? icons[id].svg : WARNING.svg);
 
-const getFillRule = id => icons[id] && !icons[id].fillRule ?
+const getFillRule = id => icons[id] && icons[id].fillRule ?
   icons[id].fillRule : '';
 
-const getViewBox = id => icons[id] && !icons[id].viewBox ?
+const getViewBox = id => icons[id] && icons[id].viewBox ?
   icons[id].viewBox : '0 0 16 16';
 
 const id = id => id ? id.replace(/-/g, '_').toUpperCase() : '';
@@ -16,11 +16,13 @@ const id = id => id ? id.replace(/-/g, '_').toUpperCase() : '';
 const wrapHtml = html => ({ __html: html });
 
 const Icon = (props) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox={getViewBox(id(props.iconId))}
-    fillRule={getFillRule(id(props.iconId))}
-    dangerouslySetInnerHTML={getSvg(id(props.iconId))} />
+  <div className="svg-icon">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox={getViewBox(id(props.iconId))}
+      fillRule={getFillRule(id(props.iconId))}
+      dangerouslySetInnerHTML={getSvg(id(props.iconId))} />
+  </div>
 )
 
 Icon.propTypes = {
