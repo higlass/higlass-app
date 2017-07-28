931e6f5e291a6b737c66a013c70af043a5b9ba8d
Fritz Lekschas <service@lekschas.de>
2017-07-28 19:27:09 -0400

    Turn into functional components

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -9,28 +9,18 @@ import ContentWrapper from '../../components/ContentWrapper/ContentWrapper';
 // Stylesheets
 import './Home.scss';
 
-// Assets
-import logo from './logo.svg';
-
-class Home extends React.Component {
-  render() {
-    return (
-      <ContentWrapper>
-        <ContentWithFooter>
-          <div className="home">
-            <div className="home-header">
-              <img src={logo} className="home-logo" alt="logo" />
-              <h2>Welcome to HiGlass</h2>
-            </div>
-            <p className="home-intro">
-              Let&apos;s build an awesome HiGlass app.
-            </p>
-          </div>
-        </ContentWithFooter>
-      </ContentWrapper>
-    );
-  }
-}
+const Home = () => (
+  <ContentWrapper>
+    <ContentWithFooter name='home'>
+      <div className='home-header'>
+        <h2>Welcome to HiGlass</h2>
+      </div>
+      <p className='home-intro'>
+        Let&apos;s build an awesome HiGlass app.
+      </p>
+    </ContentWithFooter>
+  </ContentWrapper>
+);
 
 const mapStateToProps = state => ({
   routing: state.routing,
--- src/views/NotFound/NotFound.js
+++ src/views/NotFound/NotFound.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div className="not-found">
-        <div className="not-found-header">
-          <h2>Not found</h2>
-        </div>
-        <p className="not-found-intro">
-          Sorry, this is embarrassing.
-        </p>
-      </div>
-    );
-  }
-}
+const NotFound = () => (
+  <div className="not-found">
+    <div className="not-found-header">
+      <h2>Not found</h2>
+    </div>
+    <p className="not-found-intro">
+      Sorry, this is embarrassing.
+    </p>
+  </div>
+);
 
 const mapStateToProps = state => ({
   routing: state.routing,
@@ -22,4 +18,4 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = () => ({});
 
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(NotFound);
