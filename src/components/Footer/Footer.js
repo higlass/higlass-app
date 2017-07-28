75f744e6385e9ef04d7c422509e8c4fa7a48731e
Fritz Lekschas <service@lekschas.de>
2017-07-28 16:16:23 -0400

    Optimize space of footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,24 +8,25 @@ import Icon from '../Icon/Icon';
 import './Footer.scss';
 
 const Footer = () => (
-  <footer className="footer m-t-2 p-t-2 p-b-2">
-    <div className="wrap">
-      <div className="flex-c flex-jc-c">
-        <Icon iconId="logo-hms" title="Harvard Medical School" />
-        <Icon iconId="logo-seas" title="Harvard John A. Paulson School of Engineering and Applied Sciences" />
-        <Icon iconId="logo-mit" title="Massachusetts Institute of Technology" />
+  <footer className="footer m-t-2">
+    <div className="wrap flex-c flex-a-c flex-jc-sb">
+      <div className="flex-c flex-a-c flex-v">
+        <div className="flex-c">
+          <Icon iconId="logo-hms" title="Harvard Medical School" />
+          <Icon iconId="logo-seas" title="Harvard John A. Paulson School of Engineering and Applied Sciences" />
+          <Icon iconId="logo-mit" title="Massachusetts Institute of Technology" />
+        </div>
+        <p className="copyright">&copy; 2017 <NavLink to='/about'>Harvard College</NavLink>.</p>
       </div>
 
       <nav>
-        <ul className='flex-c flex-jc-c flex-a-s no-list-style'>
+        <ul className='flex-c flex-jc-e flex-a-s no-list-style'>
           <li><NavLink to='/' activeClassName='is-active'>Home</NavLink></li>
           <li><NavLink to='/about' activeClassName='is-active'>About</NavLink></li>
           <li><NavLink to='/examples' activeClassName='is-active'>Examples</NavLink></li>
           <li><NavLink to='/docs' activeClassName='is-active'>Docs</NavLink></li>
         </ul>
       </nav>
-
-      <p className="copyright centered">Copyright &copy; 2017 the President and Fellows of Harvard College.<br/>All content on this website is licensed under the <a href="http://creativecommons.org/licenses/by/4.0/legalcode" target="_blank" rel="noopener noreferrer">Creative Commons Attribution license (CC BY)</a>. Copyright of the linked papers is with the publishers.</p>
     </div>
   </footer>
 )
