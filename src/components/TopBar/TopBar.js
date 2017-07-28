95117baa73fb480463298acac2610dad80fd2643
Fritz Lekschas <service@lekschas.de>
2017-07-28 15:39:56 -0400

    Update to `NavLink` to provide special classes on active

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 // Components
+import Hamburger from '../Hamburger/Hamburger';
 import Icon from '../Icon/Icon';
 
 import './TopBar.scss';
@@ -11,10 +12,10 @@ const TopBar = () => (
   <header className="top-bar">
     <div className="wrap flex-c flex-jc-sb">
       <div className="flex-c branding-launch">
-        <Link to='/' className="flex-c flex-a-c branding">
+        <NavLink to='/' className="flex-c flex-a-c branding">
           <Icon iconId="logo-two-tone" />
           <span className="higlass"><span className="higlass-hi">Hi</span>Glass</span>
-        </Link>
+        </NavLink>
         <a
           href="app/"
           className="btn is-uppercased icon-only"
@@ -24,9 +25,9 @@ const TopBar = () => (
       </div>
       <nav className="flex-c flex-jc-e flex-a-s is-toggable">
         <ul className="flex-c flex-jc-e flex-a-s no-list-style">
-          <li><Link to='/about'>About</Link></li>
-          <li><Link to='/examples'>Examples</Link></li>
-          <li><Link to='/docs'>Docs</Link></li>
+          <li><NavLink to='/about' activeClassName='is-active'>About</NavLink></li>
+          <li><NavLink to='/examples' activeClassName='is-active'>Examples</NavLink></li>
+          <li><NavLink to='/docs' activeClassName='is-active'>Docs</NavLink></li>
           <li className="separated-left flex-c flex-jc-c">
             <a
               href="https://github.com/hms-dbmi?&q=higlass"
@@ -37,9 +38,7 @@ const TopBar = () => (
             </a>
           </li>
         </ul>
-        <button
-          className="menu-toggler c-hamburger c-hamburger--htx"><span></span>
-        </button>
+        <Hamburger />
       </nav>
     </div>
   </header>
