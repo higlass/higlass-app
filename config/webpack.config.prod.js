30ede2e6eb1b5ed4fcc5c137dd1aec3df643888a
Fritz Lekschas <service@lekschas.de>
2017-07-28 10:27:56 -0400

    Fix webpack prod config

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -182,7 +182,7 @@ module.exports = {
       // use the "style" loader inside the async code so CSS from them won't be
       // in the main CSS file.
       {
-        test: /\.css$/,
+        test: /\.s?css$/,
         loader: ExtractTextPlugin.extract(
           Object.assign(
             {
