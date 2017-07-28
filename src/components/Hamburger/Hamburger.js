9ce0977e99a6d9ad1738867e2c736c31d338eafa
Fritz Lekschas <service@lekschas.de>
2017-07-28 15:38:48 -0400

    Add component for hamburger button

---
+++ src/components/Hamburger/Hamburger.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Hamburger.scss';
+
+const Hamburger = () => (
+  <button
+    className="menu-toggler c-hamburger c-hamburger--htx"><span></span>
+  </button>
+)
+
+export default Hamburger;
