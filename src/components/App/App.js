fdc7929136367e62e04c73e9b694a29e1302728a
Fritz Lekschas <service@lekschas.de>
2017-07-30 17:19:12 -0400

    Integrate drop notifier

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,14 +5,48 @@ import DropNotifier from '../DropNotifier/DropNotifier';
 import Main from '../Main/Main';
 import TopBar from '../TopBar/TopBar';
 
+// Utils
+import Logger from '../../utils/logger';
+
 import './App.scss';
 
-const App = () => (
-  <div className='app full-dim'>
-    <DropNotifier />
-    <TopBar />
-    <Main />
-  </div>
-);
+const logger = Logger('App');
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.dropHandler = this.dropHandler.bind(this);
+  }
+
+  dropHandler(event) {
+    const file = event.dataTransfer.files[0];
+    const reader = new FileReader();
+
+    reader.addEventListener('load', (fileEvent) => {
+      try {
+        this.setState({
+          config: JSON.parse(fileEvent.target.result),
+        });
+      } catch (e) {
+        logger.error('Only drop valid JSON', e);
+      }
+    });
+
+    reader.readAsText(file);
+  }
+
+  render() {
+    return (
+      <div className='app full-dim'>
+        <DropNotifier
+          drop={this.dropHandler} />
+        <TopBar />
+        <Main />
+      </div>
+    );
+  }
+}
 
 export default App;
