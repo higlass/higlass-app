4a1d30fed016adacb11d1291962ebe3cb89b10af
Fritz Lekschas <service@lekschas.de>
2017-07-30 19:56:30 -0400

    Add a component for centering the spinner

---
+++ src/components/SpinnerCenter/SpinnerCenter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Spinner from '../Spinner/Spinner';
+
+import './SpinnerCenter.scss';
+
+const SpinnerCenter = () => (
+  <div className='full-dim flex-c flex-a-c flex-jc-c spinner-center'>
+    <Spinner />
+  </div>
+);
+
+export default SpinnerCenter;
