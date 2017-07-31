994c1e6ddf9c6c372543d5217af62e4e91072de0
Fritz Lekschas <service@lekschas.de>
2017-07-30 22:07:33 -0400

    Style not-found page

--- src/views/NotFound/NotFound.js
+++ src/views/NotFound/NotFound.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+// Components
+import ContentWithFooter from '../../components/Content/ContentWithFooter';
+import ContentWrapper from '../../components/ContentWrapper/ContentWrapper';
+import Icon from '../../components/Icon/Icon';
+
+import './NotFound.scss';
+
 const NotFound = () => (
-  <div className="not-found">
-    <div className="not-found-header">
-      <h2>Not found</h2>
-    </div>
-    <p className="not-found-intro">
-      Sorry, this is embarrassing.
-    </p>
-  </div>
+    <ContentWrapper>
+    <ContentWithFooter name='not-found' wrap={true}>
+      <div className="flex-c flex-v flex-a-c flex-jc-c full-wh">
+        <div className='flex-c flex-v flex-a-c not-found-header'>
+          <div className="icon-wrapper"><Icon iconId='sad' /></div>
+          <h2 className='m-t-0'>Oh no&hellip; nothing found!</h2>
+        </div>
+        <em>
+          The requested page either moved or does not exist.
+        </em>
+      </div>
+    </ContentWithFooter>
+  </ContentWrapper>
 );
 
 const mapStateToProps = state => ({
