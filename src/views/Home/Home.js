34f089d9351834720dd66a4750c40ab73770e5d9
Fritz Lekschas <service@lekschas.de>
2017-07-30 22:07:56 -0400

    Use wrap prop shorthand

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 // Components
-import ContentWithFooter from '../../components/ContentWithFooter/ContentWithFooter';
+import ContentWithFooter from '../../components/Content/ContentWithFooter';
 import ContentWrapper from '../../components/ContentWrapper/ContentWrapper';
 
 // Stylesheets
@@ -11,19 +11,17 @@ import './Home.scss';
 
 const Home = () => (
   <ContentWrapper>
-    <ContentWithFooter name='home'>
-      <div className='wrap'>
-        <section className='flex-c m-t-1 m-b-2'>
-          <p className='home-intro flex-g-1'>
-          HiGlass is a tool for exploring genomic contact matrices and tracks.
-          Please take a look at the <Link to='/examples'>examples</Link> and <Link to='/docs'>documentation</Link> for a description of the ways that it can be configured to explore and
-          compare contact matrices. To load private data, HiGlass can be <a href='https://github.com/hms-dbmi/higlass/wiki#running-locally' target='_blank' rel='noopener noreferrer'>run
-          locally within a docker container</a>.
-          </p>
-        </section>
-        <div className='home-four-third'>
-          <div id='higlass' className='full-dim'></div>
-        </div>
+    <ContentWithFooter name='home' wrap={true}>
+      <section className='flex-c m-t-1 m-b-2'>
+        <p className='home-intro flex-g-1'>
+        HiGlass is a tool for exploring genomic contact matrices and tracks.
+        Please take a look at the <Link to='/examples'>examples</Link> and <Link to='/docs'>documentation</Link> for a description of the ways that it can be configured to explore and
+        compare contact matrices. To load private data, HiGlass can be <a href='https://github.com/hms-dbmi/higlass/wiki#running-locally' target='_blank' rel='noopener noreferrer'>run
+        locally within a docker container</a>.
+        </p>
+      </section>
+      <div className='home-four-third'>
+        <div id='higlass' className='full-dim'></div>
       </div>
     </ContentWithFooter>
   </ContentWrapper>
