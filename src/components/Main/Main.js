d20a911d4438980b82707987e7dc1e59b4da3213
Fritz Lekschas <service@lekschas.de>
2017-07-31 22:27:05 -0400

    Add Viewer view (for launching HiGlass)

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,9 +5,11 @@ import { withRouter } from 'react-router';
 
 // Views
 import About from '../../views/About/About';
+import Docs from '../../views/Docs/Docs';
 import Examples from '../../views/Examples/Examples';
 import Home from '../../views/Home/Home';
 import NotFound from '../../views/NotFound/NotFound';
+import Viewer from '../../views/Viewer/Viewer';
 
 // Components
 import ScrollToTop from '../ScrollToTop/ScrollToTop';
@@ -16,8 +18,24 @@ const Main = () => (
   <ScrollToTop>
     <Switch>
       <Route exact path='/about' component={About} />
+      <Route exact path='/app' render={({ location, }) => {
+        const query = new URLSearchParams(location.search);
+        const viewConfigId = query.get('config');
+
+        return <Viewer viewConfigId={viewConfigId} />;
+      }} />
       <Route exact path='/examples' component={Examples} />
-      <Route exact path='/' component={Home} />
+      <Route exact path='/docs' component={Docs} />
+      <Route exact path='/' render={({ location, }) => {
+        const query = new URLSearchParams(location.search);
+        const viewConfigId = query.get('config');
+
+        if (viewConfigId) {
+          return <Viewer viewConfigId={viewConfigId} />;
+        }
+
+        return <Home />;
+      }} />
       <Route component={NotFound}/>
     </Switch>
   </ScrollToTop>
