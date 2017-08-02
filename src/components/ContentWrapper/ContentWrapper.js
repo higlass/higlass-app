18aa0072620e114b66b764ffc4b087772ad0601d
Fritz Lekschas <service@lekschas.de>
2017-08-02 11:19:24 -0400

    Remove scroll listener

--- src/components/ContentWrapper/ContentWrapper.js
+++ src/components/ContentWrapper/ContentWrapper.js
@@ -1,15 +1,9 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-// Services
-import scroll from '../../services/scroll';
-
-import './ContentWrapper.scss';
-
 const ContentWrapper = props => (
   <div
-    className={`content-wrapper flex-c flex-v full-dim ${props.bottomBar ? 'content-wrapper-bottom-bar' : ''}`}
-    ref={el => scroll.register(el)}>
+    className={`content-wrapper flex-c flex-v full-mdim ${props.bottomBar ? 'content-wrapper-bottom-bar' : ''}`}>
     {props.children}
   </div>
 );
