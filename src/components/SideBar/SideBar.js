54561be9d3ae02b1ff7f1f1373953a681312ffa2
Fritz Lekschas <service@lekschas.de>
2017-08-01 22:43:01 -0400

    Link SideBar to scroll position

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,16 +1,72 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+// Services
+import pubSub from '../../services/pub-sub';
+
+// Utils
+import debounce from '../../utils/debounce';
+
+// Styles
 import './SideBar.scss';
 
-const SideBar = props => (
-  <aside className='side-bar'>
-    {props.children}
-  </aside>
-);
+
+class SideBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.scrollHandlerDb = debounce(this.scrollHandler.bind(this), 50);
+
+    this.sidebarOffsetTop = 0;
+    this.state = {
+      style: {
+        marginTop: 0,
+      },
+    };
+  }
+
+  componentDidMount() {
+    if (this.props.isSticky) {
+      this.sidebarOffsetTop = this.sideBarEl.getBoundingClientRect().top -
+        document.body.getBoundingClientRect().top;
+
+      pubSub.subscribe('contentScroll', this.scrollHandlerDb);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.props.isSticky) {
+      pubSub.unsubscribe('contentScroll', this.scrollHandlerDb);
+    }
+  }
+
+  render() {
+    return (
+      <aside
+        className='side-bar'
+        ref={(el) => { this.sideBarEl = el; }}
+        style={this.state.style}>
+        {this.props.children}
+      </aside>
+    );
+  }
+
+  scrollHandler(event) {
+    this.setState({
+      style: {
+        marginTop: `${event.target.scrollTop}px`,
+      },
+    });
+  }
+}
+
+SideBar.defaultProps = {
+  isSticky: false,
+};
 
 SideBar.propTypes = {
   children: PropTypes.node.isRequired,
+  isSticky: PropTypes.bool,
 };
 
 export default SideBar;
