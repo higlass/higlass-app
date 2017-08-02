cf248c2a58ff30c91c5243bd55cafb5448f7afc3
Fritz Lekschas <service@lekschas.de>
2017-08-01 22:37:19 -0400

    Link scroll publisher with content wrapper

--- src/components/ContentWrapper/ContentWrapper.js
+++ src/components/ContentWrapper/ContentWrapper.js
@@ -1,11 +1,15 @@
 import PropTypes from 'prop-types';
-
 import React from 'react';
+
+// Services
+import scroll from '../../services/scroll';
 
 import './ContentWrapper.scss';
 
 const ContentWrapper = props => (
-  <div className={`content-wrapper flex-c flex-v full-dim ${props.bottomBar ? 'content-wrapper-bottom-bar' : ''}`}>
+  <div
+    className={`content-wrapper flex-c flex-v full-dim ${props.bottomBar ? 'content-wrapper-bottom-bar' : ''}`}
+    ref={el => scroll.register(el)}>
     {props.children}
   </div>
 );
