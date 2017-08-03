3e90f7f117602d3a948667b64af0709f2722e012
Fritz Lekschas <service@lekschas.de>
2017-08-03 19:03:24 -0400

    Extend observer service to allow unsubscribing by index. Add doc strings

--- src/services/pub-sub.js
+++ src/services/pub-sub.js
@@ -1,25 +1,49 @@
 const stack = {};
 
+/**
+ * Publish an event.
+ *
+ * @param {string} event - Event type to be published.
+ * @param {any} news - The news to be published.
+ */
 const publish = (event, news) => {
   if (!stack[event]) { return; }
 
   stack[event].forEach(listener => listener(news));
 };
 
+/**
+ * Subscribe to an event.
+ *
+ * @param {string} event - Event name to subscribe to.
+ * @param {function} callback - Function to be called when event of type `event`
+ *   is published.
+ * @return {int} Index of the callback function on the event stack. The index
+ *   can be used to unsubscribe.
+ */
 const subscribe = (event, callback) => {
   if (!stack[event]) {
     stack[event] = [];
   }
 
-  stack[event].push(callback);
+  return stack[event].push(callback) - 1;
 };
 
-const unsubscribe = (event, callback) => {
+/**
+ * Unsubscribe from event.
+ *
+ * @param {string} event - Event from which to unsubscribe.
+ * @param {function} callback - Callback function to be unsubscribed. It is
+ *   ignored if `id` is provided.
+ * @param {int} id - Index of the callback function to be removed from the
+ *   event stack. The index is returned by `subscribe()`.
+ */
+const unsubscribe = (event, callback, id) => {
   if (!stack[event]) { return; }
 
-  const idx = stack[event].indexOf(callback);
+  const idx = typeof id !== 'undefined' ? id : stack[event].indexOf(callback);
 
-  if (idx === -1) { return; }
+  if (idx === -1 || idx >= stack[event].length) { return; }
 
   stack[event].splice(idx, 1);
 };
