5bf03cfc6476abc0ca552c5276e8626381f0a69b
Fritz Lekschas <service@lekschas.de>
2017-08-03 13:02:10 -0400

    Add homeInfoBarClose prop to redux state. Simplify reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,8 @@
+export const setHomeInfoBarClose = homeInfoBarClose => ({
+  type: 'SET_HOME_INFO_BAR_CLOSE',
+  payload: { homeInfoBarClose, },
+});
+
 export const setViewConfig = viewConfig => ({
   type: 'SET_VIEW_CONFIG',
   payload: { viewConfig, },
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,16 @@
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
 
-import viewConfig from './view-config';
+import defaultSetReducer from '../utils/default-set-reducer';
+
+
+export const homeInfoBarClose = defaultSetReducer('homeInfoBarClose', false);
+
+export const viewConfig = defaultSetReducer('viewConfig', null);
+
 
 export default combineReducers({
   routing,
+  homeInfoBarClose,
   viewConfig,
 });
