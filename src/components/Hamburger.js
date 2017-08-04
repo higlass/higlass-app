1d090d17848ff3cb17eb162bbf3d0d3dcb867846
Fritz Lekschas <service@lekschas.de>
2017-08-03 21:55:57 -0400

    Fix hamburger menu toggler

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,12 +1,56 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
 // Styles
 import './Hamburger.scss';
 
-const Hamburger = () => (
-  <button
-    className="menu-toggler c-hamburger c-hamburger--htx"><span></span>
-  </button>
-);
+
+class Hamburger extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isActive: false,
+    };
+  }
+
+  componentDidUpdate() {
+    this.props.onClick(this.state.isActive);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextState.isActive === this.state.isActive) { return false; }
+    return true;
+  }
+
+  render() {
+    return (
+      <button
+        className='hamburger-wrapper'
+        onClick={this.toggle.bind(this)}>
+        <div className={`hamburger hamburger-to-x ${this.state.isActive ? 'is-active' : ''}`}>
+          <span></span>
+        </div>
+        <div className='hamburger-bg' />
+      </button>
+    );
+  }
+
+  /* ------------------------------ Custom Methods -------------------------- */
+
+  toggle(event) {
+    event.preventDefault();
+
+    this.setState({
+      isActive: !this.state.isActive,
+    });
+
+    return false;
+  }
+}
+
+Hamburger.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
 
 export default Hamburger;
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -10,41 +10,64 @@ import Icon from './Icon';
 // Styles
 import './TopBar.scss';
 
-const TopBar = props => (
-  <header className='top-bar'>
-    <div className={`flex-c flex-jc-sb top-bar-wrapper ${!new URLSearchParams(props.location.search).get('config') ? 'wrap' : 'wrap-basic'}`}>
-      <div className='flex-c branding-launch'>
-        <NavLink to='/' className='flex-c flex-a-c branding'>
-          <Icon iconId='logo-two-tone' />
-          <span className='higlass'><span className='higlass-hi'>Hi</span>Glass</span>
-        </NavLink>
-        <NavLink
-          to='/?config=example'
-          className='btn is-uppercased icon-only'
-          title='Launch HiGlass in Full Screen'>
-          <Icon iconId='maximize' />
-        </NavLink>
-      </div>
-      <nav className='flex-c flex-jc-e flex-a-s is-toggable'>
-        <ul className='flex-c flex-jc-e flex-a-s no-list-style'>
-          <li><NavLink to='/about' activeClassName='is-active'>About</NavLink></li>
-          <li><NavLink to='/examples' activeClassName='is-active'>Examples</NavLink></li>
-          <li><NavLink to='/docs' activeClassName='is-active'>Docs</NavLink></li>
-          <li className='separated-left flex-c flex-jc-c'>
-            <a
-              href='https://github.com/hms-dbmi?&q=higlass'
-              target='_blank'
-              rel='noopener noreferrer'
-              className='icon-only flex-c flex-a-c'>
-              <Icon iconId='github' />
-            </a>
-          </li>
-        </ul>
-        <Hamburger />
-      </nav>
-    </div>
-  </header>
-);
+class TopBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      menuIsShown: false,
+    };
+  }
+
+  render() {
+    return (
+      <header className='top-bar'>
+        <div className={`flex-c flex-jc-sb top-bar-wrapper ${!new URLSearchParams(this.props.location.search).get('config') ? 'wrap' : 'wrap-basic'}`}>
+          <div className='flex-c branding-launch'>
+            <NavLink to='/' className='flex-c flex-a-c branding'>
+              <Icon iconId='logo-two-tone' />
+              <span className='higlass'><span className='higlass-hi'>Hi</span>Glass</span>
+            </NavLink>
+            <NavLink
+              to='/?config=example'
+              className='btn is-uppercased icon-only'
+              title='Launch HiGlass in Full Screen'>
+              <Icon iconId='maximize' />
+            </NavLink>
+          </div>
+          <nav className={`flex-c flex-jc-e flex-a-s is-toggable ${this.state.menuIsShown ? 'is-shown' : ''}`}>
+            <ul className='flex-c flex-jc-e flex-a-s no-list-style'>
+              <li><NavLink to='/about' activeClassName='is-active'>About</NavLink></li>
+              <li><NavLink to='/examples' activeClassName='is-active'>Examples</NavLink></li>
+              <li><NavLink to='/docs' activeClassName='is-active'>Docs</NavLink></li>
+              <li className='separated-left flex-c flex-jc-c'>
+                <a
+                  href='https://github.com/hms-dbmi?&q=higlass'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='icon-only flex-c flex-a-c flex-jc-c'>
+                  <Icon iconId='github' />
+                </a>
+              </li>
+            </ul>
+            <Hamburger onClick={this.toggleMenu.bind(this)} />
+          </nav>
+        </div>
+      </header>
+    );
+  }
+
+  /* ------------------------------ Custom Methods -------------------------- */
+
+  toggleMenu(isOpen) {
+    console.log('ASS', isOpen);
+    this.setState({
+      menuIsShown: isOpen,
+    });
+
+    return false;
+  }
+}
 
 TopBar.propTypes = {
   match: PropTypes.object.isRequired,
