6777e4d87edd077b807e21f34c5525ececd4ee58
Fritz Lekschas <service@lekschas.de>
2017-08-06 17:50:21 -0400

    Make bottom bar customizable

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -1,22 +1,17 @@
+import PropTypes from 'prop-types';
 import React from 'react';
-
-// Components
-import Icon from './Icon';
 
 // Styles
 import './BottomBar.scss';
 
-const BottomBar = () => (
+const BottomBar = props => (
   <footer className='flex-c flex-jc-sb bottom-bar'>
-    <ul className='flex-c flex-a-c no-list-style'>
-      <li><button>Test</button></li>
-    </ul>
-    <ul className='flex-c flex-a-c flex-jc-e no-list-style'>
-      <li><button>Test</button></li>
-      <li><button><Icon iconId='info' /></button></li>
-      <li>v1.0.0</li>
-    </ul>
+    {props.children}
   </footer>
 );
+
+BottomBar.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default BottomBar;
