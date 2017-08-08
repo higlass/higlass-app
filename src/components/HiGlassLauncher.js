bc4744d4418d2f26e1f7d63a275885278788d214
Fritz Lekschas <service@lekschas.de>
2017-08-07 21:54:11 -0400

    Unsubscribe from HiGlass events when un/re-mounted to avoid bad glitches

--- src/components/HiGlassLauncher.js
+++ src/components/HiGlassLauncher.js
@@ -16,6 +16,16 @@ const logger = Logger('HiGlassLauncher');
 
 
 class HiGlassLauncher extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.hiGlassEventListeners = [];
+  }
+
+  componentWillUnmount() {
+    this.removeHiGlassEventListeners();
+  }
+
   shouldComponentUpdate(nextProps) {
     if (deepEqual(this.newViewConfig, nextProps.viewConfig)) {
       return false;
@@ -40,26 +50,54 @@ class HiGlassLauncher extends React.Component {
     );
   }
 
+  /* ------------------------------ Custom Methods -------------------------- */
+
+  addHiGlassEventListeners() {
+    this.hiGlassEventListeners.push({
+      event: 'view',
+      id: this.api.on('view', (newViewConfig) => {
+        this.newViewConfig = JSON.parse(newViewConfig);
+
+        if (!deepEqual(this.newViewConfig, this.props.viewConfig)) {
+          this.props.setViewConfig(this.newViewConfig);
+        }
+      }),
+    });
+  }
+
   launchHgLib(viewConfig, options, onError) {
     return (element) => {
       if (element && viewConfig) {
         logger.debug(viewConfig, options);
 
         try {
-          createHgComponent(element, deepClone(viewConfig), options, (api) => {
-            api.on('view', (newViewConfig) => {
-              this.newViewConfig = JSON.parse(newViewConfig);
-              if (!deepEqual(this.newViewConfig, viewConfig)) {
-                this.props.setViewConfig(this.newViewConfig);
-              }
-            });
-          });
+          createHgComponent(
+            element,
+            deepClone(viewConfig),
+            options,
+            this.registerHiGlassApi.bind(this)
+          );
         } catch (error) {
           logger.error(error);
           onError('HiGlass could not be launched.');
         }
       }
     };
+  }
+
+  registerHiGlassApi(api) {
+    if (this.api) {
+      this.removeHiGlassEventListeners();
+    }
+    this.api = api;
+    this.addHiGlassEventListeners();
+  }
+
+  removeHiGlassEventListeners() {
+    this.hiGlassEventListeners.forEach((listener) => {
+      this.api.off(listener.event, listener.id);
+    });
+    this.hiGlassEventListeners = [];
   }
 }
 
