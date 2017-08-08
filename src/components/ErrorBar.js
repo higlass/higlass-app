b4031390d06e2ed46cef0eb13536d9ec3d771148
Fritz Lekschas <service@lekschas.de>
2017-08-08 11:30:00 -0400

    Close / hide error bar on ESC

--- src/components/ErrorBar.js
+++ src/components/ErrorBar.js
@@ -5,27 +5,59 @@ import React from 'react';
 import ButtonIcon from './ButtonIcon';
 import Icon from './Icon';
 
+// Services
+import pubSub from '../services/pub-sub';
+
 // Styles
 import './ErrorBar.scss';
 
-const ErrorBar = props => (
-  <header className='error-bar rel'>
-    <div className={`flex-c flex-a-c flex-jc-sb ${props.wrap ? 'wrap' : ''}`}>
-      <div className='flex-c flex-a-c error-bar-content'>
-        <Icon iconId='warning' />
-        <p className='error-bar-msg'>{props.msg}</p>
-      </div>
-      {props.isClosable &&
-        <div className='flex-c flex-a-c flex-jc-c rel error-bar-close'>
-          <ButtonIcon
-            icon='cross'
-            iconOnly={true}
-            onClick={props.onClose} />
+class ErrorBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.pubSubs = [];
+  }
+
+  componentWillMount() {
+    this.pubSubs.push(
+      pubSub.subscribe('keyup', this.keyUpHandler.bind(this))
+    );
+  }
+
+  componentWillUnmount() {
+    this.pubSubs.forEach(subscription => pubSub.unsubscribe(subscription));
+    this.pubSubs = [];
+  }
+
+  render() {
+    return (
+      <header className='error-bar rel'>
+        <div className={`flex-c flex-a-c flex-jc-sb ${this.props.wrap ? 'wrap' : ''}`}>
+          <div className='flex-c flex-a-c error-bar-content'>
+            <Icon iconId='warning' />
+            <p className='error-bar-msg'>{this.props.msg}</p>
+          </div>
+          {this.props.isClosable &&
+            <div className='flex-c flex-a-c flex-jc-c rel error-bar-close'>
+              <ButtonIcon
+                icon='cross'
+                iconOnly={true}
+                onClick={this.props.onClose} />
+            </div>
+          }
         </div>
-      }
-    </div>
-  </header>
-);
+      </header>
+    );
+  }
+
+  /* ------------------------------ Custom Methods -------------------------- */
+
+  keyUpHandler(event) {
+    if (event.keyCode === 27) {  // ESC
+      this.props.onClose();
+    }
+  }
+}
 
 ErrorBar.propTypes = {
   isClosable: PropTypes.bool,
