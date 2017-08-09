27ab60eb7432426e064938cd30ef5ab36bcd2591
Fritz Lekschas <service@lekschas.de>
2017-08-08 21:48:59 -0400

    Add components for drop down

---
+++ src/components/DropDown.js
@@ -0,0 +1,63 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+class DropDown extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: false,
+    };
+  }
+
+  render() {
+    const childrenWithProps = React.Children.map(this.props.children,
+      child => React.cloneElement(child, {
+        dropDownIsOpen: this.state.isOpen,
+        dropDownToggle: this.toggle.bind(this),
+      })
+    );
+
+    let className = 'rel drop-down';
+
+    className += this.state.isOpen ? ' drop-down-is-open' : '';
+    className += this.props.alignRight ? ' drop-down-align-right' : '';
+    className += this.props.alignTop ? ' drop-down-align-top' : '';
+
+    return (
+      <div className={className}>
+        {childrenWithProps}
+      </div>
+    );
+  }
+
+  /* ------------------------------ Custom Methods -------------------------- */
+
+  close() {
+    this.setState({
+      isOpen: false,
+    });
+  }
+
+  open() {
+    this.setState({
+      isOpen: true,
+    });
+  }
+
+  toggle() {
+    if (this.state.isOpen) {
+      this.close();
+    } else {
+      this.open();
+    }
+  }
+}
+
+DropDown.propTypes = {
+  children: PropTypes.node.isRequired,
+  alignRight: PropTypes.bool,
+  alignTop: PropTypes.bool,
+};
+
+export default DropDown;
