4fa3e8e6997a35ee3e99bd3ee982833a464f5f9c
Fritz Lekschas <service@lekschas.de>
2017-08-08 21:47:35 -0400

    Support custom class names and add an ability for generic activity state

--- src/components/ButtonIcon.js
+++ src/components/ButtonIcon.js
@@ -9,7 +9,7 @@ import './ButtonIcon.scss';
 
 const ButtonIcon = props => (
   <button
-    className={`flex-c flex-a-c flex-jc-c button-icon ${props.iconOnly ? 'button-icon-only' : ''}`}
+    className={`flex-c flex-a-c flex-jc-c button-icon ${props.iconOnly ? 'button-icon-only' : ''} ${props.isActive ? 'is-active' : ''} ${props.className}`}
     title={props.title}
     onClick={props.onClick}>
     <Icon
@@ -22,10 +22,12 @@ const ButtonIcon = props => (
 
 ButtonIcon.propTypes = {
   children: PropTypes.node,
+  className: PropTypes.string,
   icon: PropTypes.string.isRequired,
   iconMirrorH: PropTypes.bool,
   iconMirrorV: PropTypes.bool,
   iconOnly: PropTypes.bool,
+  isActive: PropTypes.bool,
   onClick: PropTypes.func,
   title: PropTypes.string,
 };
