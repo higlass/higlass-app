3205abf5b76bc5cdb6bf5ef55d75249f4967af8c
Fritz Lekschas <service@lekschas.de>
2017-08-10 10:17:44 -0400

    Support sub-top and right bar

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,17 +4,36 @@ import React from 'react';
 // Styles
 import './Content.scss';
 
+const classNames = (props) => {
+  let className = 'flex-g-1 content';
+
+  className += ` ${props.name}-content`;
+  className += props.hasRightBar ? ' has-right-bar' : '';
+  className += props.rel ? ' rel' : '';
+  className += props.wrap ? ' wrap' : '';
+
+  return className;
+};
+
+const styles = props => ({
+  marginRight: `${props.rightBarShow ? props.rightBarWidth : 0}px`,
+});
+
 const Content = props => (
   <main
-    className={`flex-g-1 content ${props.name}-content ${props.wrap ? 'wrap' : ''} ${props.rel ? 'rel' : ''}`}>
+    className={classNames(props)}
+    style={styles(props)}>
     {props.children}
   </main>
 );
 
 Content.propTypes = {
   children: PropTypes.node,
+  hasRightBar: PropTypes.bool,
   name: PropTypes.string.isRequired,
   rel: PropTypes.bool,
+  rightBarShow: PropTypes.bool,
+  rightBarWidth: PropTypes.number,
   wrap: PropTypes.bool,
 };
 
