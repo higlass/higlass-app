488d210b7061c3ef3552b9b8fc42db566f2546b3
Fritz Lekschas <service@lekschas.de>
2017-08-10 10:15:22 -0400

    Add support for storing visible state of the right side bar

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,4 +12,14 @@ export const setViewConfig = viewConfig => ({
   payload: { viewConfig },
 });
 
+export const setViewerRightBarWidth = viewerRightBarWidth => ({
+  type: 'SET_VIEWER_RIGHT_BAR_WIDTH',
+  payload: { viewerRightBarWidth },
+});
+
+export const setViewerRightBarShow = viewerRightBarShow => ({
+  type: 'SET_VIEWER_RIGHT_BAR_SHOW',
+  payload: { viewerRightBarShow },
+});
+
 export const undo = ActionCreators.undo();
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -8,9 +8,15 @@ export const homeInfoBarClose = defaultSetReducer('homeInfoBarClose', false);
 
 export const viewConfig = defaultSetReducer('viewConfig', null);
 
+export const viewerRightBarShow = defaultSetReducer('viewerRightBarShow', false);
+
+export const viewerRightBarWidth = defaultSetReducer('viewerRightBarWidth', 200);
+
 
 export default combineReducers({
   routing,
   homeInfoBarClose,
   viewConfig,
+  viewerRightBarShow,
+  viewerRightBarWidth,
 });
