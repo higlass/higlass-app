1c1e7403aa76d4d2d811aa387d2f58012aac4a41
Fritz Lekschas <service@lekschas.de>
2017-08-12 21:23:02 -0400

    Add buttons choosing the mouse tool

--- src/views/ViewerSubTopBar.js
+++ src/views/ViewerSubTopBar.js
@@ -8,6 +8,9 @@ import SubTopBar from '../components/SubTopBar';
 import SubTopBottomBarButtons from '../components/SubTopBottomBarButtons';
 import ToolTip from '../components/ToolTip';
 
+// Actions
+import { setViewerMouseTool } from '../actions';
+
 // Utils
 import downloadAsJson from '../utils/download-as-json';
 
@@ -17,14 +20,57 @@ const downloadViewConfig = props =>
 const ViewerSubTopBar = props => (
   <SubTopBar>
     <SubTopBottomBarButtons
-      className='flex-c flex-a-c no-list-style' />
+      className='flex-c flex-a-c no-list-style'>
+      <li>
+        <ToolTip
+          align='left'
+          delayIn={1000}
+          delayOut={500}
+          title={
+            <span className='flex-c'>
+              <span>Pan & Zoom Tool</span>
+              <span className='short-cut'>Z</span>
+            </span>
+          }>
+          <ButtonIcon
+            icon='drag'
+            iconOnly={true}
+            isActive={props.viewerMouseTool === 'panZoom'}
+            onClick={() => props.setViewerMouseTool('panZoom')} />
+        </ToolTip>
+      </li>
+      <li>
+        <ToolTip
+          align='left'
+          delayIn={1000}
+          delayOut={500}
+          title={
+            <span className='flex-c'>
+              <span>Select Tool</span>
+              <span className='short-cut'>S</span>
+            </span>
+          }>
+          <ButtonIcon
+            icon='select'
+            iconOnly={true}
+            isActive={props.viewerMouseTool === 'select'}
+            onClick={() => props.setViewerMouseTool('select')} />
+        </ToolTip>
+      </li>
+    </SubTopBottomBarButtons>
     <SubTopBottomBarButtons
       className='flex-c flex-a-c flex-jc-e no-list-style'>
       <li>
         <ToolTip
+          align='right'
           delayIn={1000}
           delayOut={500}
-          title='Download view config as JSON'>
+          title={
+            <span className='flex-c'>
+              <span>Download View Config</span>
+              <span className='short-cut'>CMD + S</span>
+            </span>
+          }>
           <ButtonIcon
             icon='download'
             iconOnly={true}
@@ -36,14 +82,20 @@ const ViewerSubTopBar = props => (
 );
 
 ViewerSubTopBar.propTypes = {
+  setViewerMouseTool: PropTypes.func,
   viewConfig: PropTypes.object,
+  viewerMouseTool: PropTypes.string,
 };
 
 const mapStateToProps = state => ({
   viewConfig: state.present.viewConfig,
+  viewerMouseTool: state.present.viewerMouseTool,
 });
 
-const mapDispatchToProps = () => ({});
+const mapDispatchToProps = dispatch => ({
+  setViewerMouseTool: viewerMouseTool =>
+    dispatch(setViewerMouseTool(viewerMouseTool)),
+});
 
 export default connect(
   mapStateToProps,
