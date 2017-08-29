1e0846f66042538f04a70e23720ee6c2d0f67761
Fritz Lekschas <service@lekschas.de>
2017-08-29 12:03:35 -0400

    Add index for convenience

---
+++ src/utils/index.js
@@ -0,0 +1,31 @@
+export { default as camelToConst } from './camel-to-const';
+
+export { default as cookie } from './cookie';
+
+export { default as debounce } from './debounce';
+
+export { default as deepClone } from './deep-clone';
+
+export { default as defaultSetReducer } from './default-set-reducer';
+
+export { default as deferred } from './deferred';
+
+export { default as downloadAsFile } from './download-as-file';
+
+export { default as downloadAsJson } from './download-as-json';
+
+export { default as hasParent } from './has-parent';
+
+export { default as loadViewConfig } from './load-view-config';
+
+export { default as Logger } from './logger';
+
+export { default as multiStorage } from './multi-storage';
+
+export { default as requestAnimationFrame } from './request-animation-frame';
+
+export { default as cancelAnimationFrame } from './request-animation-frame';
+
+export { default as requestNextAnimationFrame } from './request-animation-frame';
+
+export { default as cancelNextAnimationFrame } from './request-animation-frame';
