be72ba567e55209d860b986de1c5a5d622d8171a
Fritz Lekschas <service@lekschas.de>
2017-11-19 15:32:09 -0500

    Use public docs

--- src/views/Docs.js
+++ src/views/Docs.js
@@ -4,30 +4,50 @@ import React from 'react';
 import Content from '../components/Content';
 import ContentWrapper from '../components/ContentWrapper';
 import Footer from '../components/Footer';
-import SideBar from '../components/SideBar';
 
-// Docs
-import SideBarNav from '../sidebar.md';
-import Wiki from '../wiki.md';
+// Utils
+import {
+  requestNextAnimationFrame
+} from '../utils';
 
-// Stylesheets
-import './Docs.scss';
+class Docs extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = { height: '85vh' };
+  }
 
-const Docs = () => (
-  <ContentWrapper name='docs'>
-    <Content name='docs'>
-      <div className='flex-c flex-g-1 wrap p-t-1 p-b-1'>
-        <div className="rel column-3-4 wiki">
-          <Wiki />
-        </div>
-        <div className="rel column-1-4">
-          <SideBar isSticky={true}><SideBarNav /></SideBar>
-        </div>
-      </div>
-    </Content>
-    <Footer />
-  </ContentWrapper>
-);
+  getHeight(el) {
+    requestNextAnimationFrame(() => {
+      if (!el) return;
+
+      const height = `${el.getBoundingClientRect().height}px`;
+
+      if (this.state.height !== height) {
+        this.setState({
+          height: `${el.getBoundingClientRect().height}px`,
+        });
+      }
+    });
+  }
+
+  render() {
+    return (
+      <ContentWrapper name='docs'>
+        <Content name='docs' rel={true} bottomMargin={false}>
+          <div className='full-dim oh' ref={this.getHeight.bind(this)}>
+            <iframe
+              className='full-wh'
+              frameBorder='0'
+              src='https://hms-dbmi.github.io/higlass-docs/'
+              style={{ height: this.state.height }}
+            />
+          </div>
+        </Content>
+        <Footer />
+      </ContentWrapper>
+    );
+  }
+}
 
 export default Docs;
