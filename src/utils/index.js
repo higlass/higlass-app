f597906133b1948cee1fb1b690cbf211971c07d8
Fritz Lekschas <service@lekschas.de>
2018-01-04 16:14:42 +0100

    Add utility function for flattening an array

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,25 +1,15 @@
 export { default as camelToConst } from './camel-to-const';
-
 export { default as cookie } from './cookie';
-
 export { default as debounce } from './debounce';
-
 export { default as deepClone } from './deep-clone';
-
 export { default as defaultSetReducer } from './default-set-reducer';
-
 export { default as deferred } from './deferred';
-
 export { default as downloadAsFile } from './download-as-file';
-
 export { default as downloadAsJson } from './download-as-json';
-
+export { default as flatten } from './flatten';
 export { default as hasParent } from './has-parent';
-
 export { default as loadViewConfig } from './load-view-config';
-
 export { default as Logger } from './logger';
-
 export { default as multiStorage } from './multi-storage';
 
 export {
