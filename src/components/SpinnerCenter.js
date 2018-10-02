4ff587cb81b4e2ee58fcc9047b2207fcd0f72300
Fritz Lekschas <service@lekschas.de>
2018-10-01 20:59:30 -0400

    Fix broken animation. Add light style.

--- src/components/SpinnerCenter.js
+++ src/components/SpinnerCenter.js
@@ -9,12 +9,13 @@ import './SpinnerCenter.scss';
 
 const SpinnerCenter = props => (
   <div className='full-dim flex-c flex-a-c flex-jc-c spinner-center'>
-    <Spinner delayed={props.delayed} />
+    <Spinner delayed={props.delayed} light={props.light} />
   </div>
 );
 
 SpinnerCenter.propTypes = {
   delayed: PropTypes.bool,
+  light: PropTypes.bool,
 };
 
 export default SpinnerCenter;
