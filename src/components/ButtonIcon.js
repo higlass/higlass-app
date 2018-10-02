c6162b1114a2569612b4cde86ec9973ddb861092
Fritz Lekschas <service@lekschas.de>
2018-10-01 20:06:23 -0400

    Fix undefined class issue. Cross update peax

--- src/components/ButtonIcon.js
+++ src/components/ButtonIcon.js
@@ -10,9 +10,13 @@ import './ButtonIcon.scss';
 const classNames = (props) => {
   let className = 'flex-c flex-a-c flex-jc-c button-icon';
 
-  className += ` ${props.className}`;
+  className += ` ${props.className || ''}`;
   className += props.iconOnly ? ' button-icon-only' : '';
   className += props.isActive ? ' is-active' : '';
+  className += props.isDisabled ? ' is-disabled' : '';
+  className += props.isIconMirrorOnFocus ? ' is-icon-mirror-on-focus' : '';
+  className += props.isIconRotationOnFocus ? ' is-icon-rotation-on-focus' : '';
+  className += props.iconPosition === 'right' ? ' flex-rev' : '';
 
   return className;
 };
@@ -29,14 +33,23 @@ const ButtonIcon = (props) => {
       title={props.title}
       onClick={props.onClick}
       onMouseDown={props.onMouseDown}
-      onMouseUp={props.onMouseUp}>
-      <Icon
-        iconId={props.icon}
-        mirrorH={props.iconMirrorH}
-        mirrorV={props.iconMirrorV} />
+      onMouseUp={props.onMouseUp}
+      disabled={props.isDisabled}
+    >
+      {!!props.icon && (
+        <Icon
+          iconId={props.icon}
+          mirrorH={props.iconMirrorH}
+          mirrorV={props.iconMirrorV}
+        />
+      )}
       <span>{props.children}</span>
     </Tag>
   );
+};
+
+ButtonIcon.defaultProps = {
+  iconPosition: 'left',
 };
 
 ButtonIcon.propTypes = {
@@ -44,11 +57,15 @@ ButtonIcon.propTypes = {
   className: PropTypes.string,
   external: PropTypes.bool,
   href: PropTypes.string,
-  icon: PropTypes.string.isRequired,
+  icon: PropTypes.string,
   iconMirrorH: PropTypes.bool,
   iconMirrorV: PropTypes.bool,
   iconOnly: PropTypes.bool,
+  iconPosition: PropTypes.oneOf(['left', 'right']),
   isActive: PropTypes.bool,
+  isDisabled: PropTypes.bool,
+  isIconMirrorOnFocus: PropTypes.bool,
+  isIconRotationOnFocus: PropTypes.bool,
   onClick: PropTypes.func,
   onMouseDown: PropTypes.func,
   onMouseUp: PropTypes.func,
