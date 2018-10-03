767fea878d292718df9293b04a6ecfe6ee8ddeb5
Fritz Lekschas <service@lekschas.de>
2018-10-03 11:13:01 -0400

    Fix regrassion from updating to react scripts v2

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -489,7 +489,7 @@ module.exports = {
     // Generates an `index.html` file with the <script> injected.
     new HtmlWebpackPlugin({
       inject: true,
-      template: paths.appHtml,
+      template: paths.appProdHtml,
       minify: {
         removeComments: true,
         collapseWhitespace: true,
