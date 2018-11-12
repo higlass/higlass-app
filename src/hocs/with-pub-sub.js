43e15f872c7ef44696ac30651d4338c6c2ad986c
Fritz Lekschas <service@lekschas.de>
2018-11-12 11:50:04 -0500

    Forward `ref` of wrapped components

--- src/hocs/with-pub-sub.js
+++ src/hocs/with-pub-sub.js
@@ -3,11 +3,11 @@ import React from 'react';
 const { Provider, Consumer } = React.createContext();
 
 // Higher order component
-const withPubSub = Component => props => (
+const withPubSub = Component => React.forwardRef((props, ref) => (
   <Consumer>
-    {pubSub => <Component {...props} pubSub={pubSub} />}
+    {pubSub => <Component ref={ref} {...props} pubSub={pubSub} />}
   </Consumer>
-);
+));
 
 export default withPubSub;
 
