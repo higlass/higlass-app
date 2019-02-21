a39b53081193c513d9f3ab642093196e69774f12
Fritz Lekschas <service@lekschas.de>
2019-02-21 17:02:09 -0500

    Add option for showing custom footer of the organization running HiGlass

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,13 +3,94 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 // Components
+import ButtonLikeLink from './ButtonLikeLink'
 import Icon from './Icon';
 
 // Styles
 import './Footer.scss';
 
+const runByOrganization = typeof window.HGAC_RUN_BY_ORGANIZATION === "string"
+  ? window.HGAC_RUN_BY_ORGANIZATION // from compiled `config.js`
+  : null;
+
+const runByOrganizationUrl = typeof window.HGAC_RUN_BY_ORGANIZATION_URL === "string"
+  ? window.HGAC_RUN_BY_ORGANIZATION_URL // from compiled `config.js`
+  : null;
+
+const runBySlogan = typeof window.HGAC_RUN_BY_SLOGAN === "string"
+  ? window.HGAC_RUN_BY_SLOGAN // from compiled `config.js`
+  : null;
+
+const runByLogoUrl = typeof window.HGAC_RUN_BY_LOGO_URL === "string"
+  ? window.HGAC_RUN_BY_LOGO_URL // from compiled `config.js`
+  : null;
+
+const runByLogoOnly = !!window.HGAC_RUN_BY_LOGO_ONLY;
+
+const runByContactUrl = typeof window.HGAC_RUN_BY_CONTACT_URL === "string"
+  ? window.HGAC_RUN_BY_CONTACT_URL // from compiled `config.js`
+  : null;
+
 const Footer = () => (
   <footer className="footer">
+    {runByOrganization && (
+      <div id="run-by">
+        <div className="wrap flex-c flex-v">
+          <div className="flex-c run-by-slogan">
+            {runBySlogan || 'Run by'}
+           </div>
+          <div className="flex-c flex-a-c flex-jc-sb">
+            <div
+              className="flex-c flex-a-c"
+            >
+              {runByLogoUrl && (
+                <img
+                  src={runByLogoUrl}
+                  alt={`${runByOrganization} Logo`}
+                  className="run-by-logo"
+                  style={{ width: runByLogoOnly ? 'auto' : null }}
+                />
+              )}
+              {(!runByLogoUrl || !runByLogoOnly) && (
+                <span
+                  className="run-by-organization"
+                  style={{
+                    marginBottom: !runByLogoUrl ? 0: null
+                  }}
+                >
+                {runByOrganizationUrl ? (
+                  <a
+                    href={runByOrganizationUrl}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    {runByOrganization}
+                  </a>
+                ) : (
+                  {runByOrganization}
+                )}
+                </span>
+              )}
+            </div>
+            <div>
+              {runByContactUrl && (
+                <ButtonLikeLink
+                  to={runByContactUrl}
+                  external={true}
+                  newWindow={true}
+                  className="run-by-contact"
+                  style={{
+                    marginBottom: !runByLogoUrl ? 0: null
+                  }}
+                >
+                  Contact
+                </ButtonLikeLink>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    )}
     <div className="wrap flex-c flex-a-c flex-jc-sb">
       <div className="flex-c flex-a-c flex-v">
         <div className="flex-c">
