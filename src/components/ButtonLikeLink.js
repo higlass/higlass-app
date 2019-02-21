d8cee939dcaee694116f7a324028c8e2658950f9
Fritz Lekschas <service@lekschas.de>
2019-02-21 17:01:05 -0500

    Support external and mailto links

--- src/components/ButtonLikeLink.js
+++ src/components/ButtonLikeLink.js
@@ -5,20 +5,38 @@ import { Link } from 'react-router-dom';
 // Styles
 import './ButtonLikeLink.scss';
 
+const isMailTo = url => url.substr(0, 7) === 'mailto:';
+
 const ButtonLikeLink = props => (
-  <div className={`flex-c flex-a-c button-like-link ${props.className}`}>
-    <Link to={props.to}>{props.children}</Link>
+  <div className={`flex-c flex-a-c button-like-link ${props.className}`} style={props.style}>
+    {props.external ? (
+      <a
+        href={props.to}
+        target={props.newWindow && !isMailTo(props.to) ? '_blank' : ''}
+        rel={props.newWindow && !isMailTo(props.to) ? 'noopener noreferrer' : ''}
+      >
+        {props.children}
+      </a>
+    ) : (
+      <Link to={props.to}>{props.children}</Link>
+    )}
   </div>
 );
 
 ButtonLikeLink.defaultProps = {
   className: '',
+  external: false,
+  newWindow: false,
+  style: {},
 };
 
 ButtonLikeLink.propTypes = {
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
   to: PropTypes.string.isRequired,
+  external: PropTypes.bool,
+  newWindow: PropTypes.bool,
+  style: PropTypes.obj,
 };
 
 export default ButtonLikeLink;
